0e27d49b9cb12efd470eec52025d646bb06425b9
KISANPARVARISH <aroraankita443@gmail.com>
2020-12-25 16:46:36 +0530

    initial

--- KP/views.py
+++ KP/views.py
@@ -6,5 +6,10 @@ from django.core.files.storage import FileSystemStorage
 def home(request):
     return render(request, "home.html")
 
+
 def dashboard(request):
     return render(request, "dashboard.html")
+
+
+def sdashboard(request):
+    return render(request, "sdashboard.html")
